d1773881fc8bd11929f89ef6d79cc48def0569fa
purnasankar300 <75303891+purnasankar300@users.noreply.github.com>
2021-04-17 18:53:39 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import time
 
 
 def predict_sentiment(user_id,recc_df):
     predict_df=pd.read_csv('sample30_tfidf_predict.csv',index_col='Product')
     dataframe_df=predict_df[predict_df.index.isin(recc_df.loc[user_id].sort_values(ascending=False)[0:20].index)]
+    time.sleep(6)
     return dataframe_df 
